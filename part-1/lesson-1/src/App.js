0177c54e59bb7755dfe8734ace32387519754356
Sahithi Pyneni <sahithipyneni24@gmail.com>
2023-01-09 20:47:43 +0530

    Till part one

---
+++ part-1/lesson-1/src/App.js
@@ -0,0 +1,43 @@
+const Header=props=>(
+<h1>{props.course}</h1>
+)
+const Content = props => (
+  <>
+
+<p>
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
+</>
+)
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content 
+       part1={part1}
+       part2={part2} 
+       part3={part3} 
+       exercise1={exercises1}
+       exercise2={exercises2} 
+       exercise3={exercises3}
+       />
+      <p>Number of exercise {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  )
+}
+
+export default App
